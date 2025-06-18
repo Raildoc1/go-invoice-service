aa0fba333ef1d1fe9f57476c96cfe9bc346c1489
Kirill Gaiduk <raildoc@Kirills-MacBook-Pro.local>
2025-06-18 21:30:12 +0300

    wip

---
+++ sh/generate-grpc.sh
@@ -0,0 +1,1 @@
+find proto -name "*.proto" -exec protoc --proto_path=proto --go_out=common/protocol/proto --go-grpc_out=.. --go_opt=paths=source_relative {} +
---
+++ sh/generate-sqlc.sh
@@ -0,0 +1,1 @@
+docker run --rm -v ./:/src -w /src sqlc/sqlc generate
